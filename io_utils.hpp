fa7fda28b96c24e10b1909e81b99088b75ab446b
Kenta Shimazaki <shiba@Mac.mshome.net>
2025-10-18 14:32:15 +0900

    fix: かけていたファイルを追加

---
+++ io_utils.hpp
@@ -0,0 +1,13 @@
+// ファイル名: io_utils.hpp
+
+#ifndef IO_UTILS_HPP
+#define IO_UTILS_HPP
+
+#include <Eigen/Sparse>
+#include <string>
+
+// 関数の「宣言」のみを記述。中身({})は書かない。
+Eigen::SparseMatrix<double> readSparseMatrixFromFile(
+    const std::string& filename);
+
+#endif  // IO_UTILS_HPP
